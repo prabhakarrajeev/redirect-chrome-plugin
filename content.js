647eb6ee7db2fa96e6950a0400968721950f88f9
Rajeev Prabhakar <rprabhakar@quantcast.com>
2017-10-09 18:33:10 -0700

    code cleanup

--- content.js
+++ content.js
@@ -1,9 +1,12 @@
-var amazon="https://www.amazon.com/";
-var amazonRedirect = "https://trueblue.jetblue.com/group/amazonOffer";
+var redirects = {
+  "https://www.amazon.com/":"https://trueblue.jetblue.com/group/amazonOffer",
+  "https://www.airbnb.com/":"https://www.deltaairbnb.com/"
+};
 
 console.log(window.document.title)
 
-if (amazon == window.location.href) // if it matches pattern defined above
-{
-  chrome.extension.sendRequest({redirect: amazonRedirect}); // send message to redirect
+// if it matches pattern defined above
+if( redirects[window.location.href] ) {
+  // send message to redirect
+  chrome.extension.sendRequest({redirect: redirects[window.location.href]});
 }
